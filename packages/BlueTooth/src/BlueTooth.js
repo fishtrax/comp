13fd56ec2af87b075e6294ac20e3caa10c5a903d
Shawn <shawn.brinkman@alayo.io>
2021-08-02 12:42:02 -0600

    added switch

--- packages/BlueTooth/src/BlueTooth.js
+++ packages/BlueTooth/src/BlueTooth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 //import {Puck} from './Puck';
-import { withStyles } from '@material-ui/core/styles';
 const Puck = require('./Puck.js')
 import Button from '@material-ui/core/Button';
+import Chart from "react-apexcharts";
+import { withStyles } from '@material-ui/core/styles';
+
 
 const styles = {
     button: {
@@ -29,6 +31,9 @@ const styles = {
           console.log("Received: ",line);
         }
       }
+
+
+     
 
 console.log(Puck)
 
@@ -71,16 +76,167 @@ let connection;
     }   
 
 class BlueTooth extends Component {
+    constructor(props) {
+        super(props);
+    this.state = {
+        optionsRadial: {
+          plotOptions: {
+            radialBar: {
+              startAngle: -135,
+              endAngle: 225,
+              hollow: {
+                margin: 0,
+                size: "70%",
+                background: "#fff",
+                image: undefined,
+                imageOffsetX: 0,
+                imageOffsetY: 0,
+                position: "front",
+                dropShadow: {
+                  enabled: true,
+                  top: 3,
+                  left: 0,
+                  blur: 4,
+                  opacity: 0.24
+                }
+              },
+              track: {
+                background: "#fff",
+                strokeWidth: "67%",
+                margin: 0, // margin is in pixels
+                dropShadow: {
+                  enabled: true,
+                  top: -3,
+                  left: 0,
+                  blur: 4,
+                  opacity: 0.35
+                }
+              },
+  
+              dataLabels: {
+                showOn: "always",
+                name: {
+                  offsetY: -20,
+                  show: true,
+                  color: "#999",
+                  fontSize: "13px"
+                },
+                value: {
+                  formatter: function (val) {
+                    return val;
+                  },
+                  color: "#999",
+                  fontSize: "30px",
+                  show: true
+                }
+              }
+            }
+          },
+          fill: {
+            type: "solid",
+            gradient: {
+              shade: "dark",
+              type: "horizontal",
+              shadeIntensity: 0.5,
+              gradientToColors: ["#ff0000"],
+              opacityFrom: 1,
+              opacityTo: 1,
+              stops: [0, 100]
+            }
+          },
+          stroke: {
+            lineCap: "round"
+          },
+          labels: ["Battery"]
+        },
+        seriesRadial: [0],
+        optionsBar: {
+          chart: {
+            stacked: true,
+            stackType: "100%",
+            toolbar: {
+              show: false
+            }
+          },
+          plotOptions: {
+            bar: {
+              horizontal: true
+            }
+          },
+          dataLabels: {
+            dropShadow: {
+              enabled: true
+            }
+          },
+          stroke: {
+            width: 0
+          },
+         
+          fill: {
+            opacity: 1,
+            type: "gradient",
+            gradient: {
+              shade: "dark",
+              type: "vertical",
+              shadeIntensity: 0.35,
+              gradientToColors: undefined,
+              inverseColors: false,
+              opacityFrom: 0.45,
+              opacityTo: 0.65,
+              stops: [90, 0, 100]
+            }
+          },
+  
+          legend: {
+            position: "bottom",
+            horizontalAlign: "right"
+          }
+        },
+        
+      };
 
+    }
+
+    puck = ()=>{
+        let _this = this;
+        Puck.eval("{bat:Puck.getBatteryPercentage()}", function(d,err) {
+            if (!d) {
+              alert("Web Bluetooth connection failed!\n"+(err||""));
+              return;
+            }
+
+            console.log(d)
+            // _this.setState({ 
+            //     seriesRadial:d.bat
+            // })
+            // Puck.eval("history", function(d) {
+            //     this.setState({ 
+            //         seriesRadial:d
+            //     })
+            // });
+          });
+
+    }
 
 
     render() {
-
+        
     const { classes } = this.props;
 
       return (
         <div className="App">
-          <Button onClick={connectDevice()}>Connect</Button>
+          <Button onClick={connectDevice}>Connect</Button>
+          <Button onClick={this.puck}>puck</Button>
+          <div>
+          <Chart
+              options={this.state.optionsRadial}
+              series={this.state.seriesRadial}
+              type="radialBar"
+              width="280"
+            />
+         
+           
+          </div>
         </div>
       );
     }
---
+++ packages/Switch/src/Switch.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const FishtraxSwitch = withStyles((theme) => ({
+    root: {
+        width: 42,
+        height: 26,
+        padding: 0,
+        margin: theme.spacing(1),
+    },
+  switchBase: {
+    padding: 1,
+    '&$checked': {
+      transform: 'translateX(16px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        backgroundColor: '#52d869',
+        opacity: 1,
+        border: 'none',
+      },
+    },
+    '&$focusVisible $thumb': {
+      color: '#52d869',
+      border: '6px solid #fff',
+    },
+  },
+  thumb: {
+    width: 24,
+    height: 24,
+  },
+  track: {
+    borderRadius: 26 / 2,
+    border: `1px solid ${theme.palette.grey[400]}`,
+    backgroundColor: theme.palette.grey[50],
+    opacity: 1,
+    transition: theme.transitions.create(['background-color', 'border']),
+  },
+  checked: {},
+}))(Switch);
+
+export default function SwitchButton() {
+  const [state, setState] = React.useState({
+    checkedC: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormGroup>
+       <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <FishtraxSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>Conect</Grid>
+        </Grid>
+      </Typography>
+    </FormGroup>
+  );
+}
